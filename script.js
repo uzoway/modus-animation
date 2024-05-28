1920b76273cae750958619d8089f91cbd1e0d88a
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2024-05-28 23:56:59 +0100

    feat: Add the ability to toggle the grid overlay using shift + g

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Uncomment to be able to toggle the grid visualizer
-document.querySelector("body").addEventListener("click", () => {
-    document.querySelector(".overlay").classList.toggle("active");
-})
-
-
-
 gsap.registerPlugin(CustomEase);
 // CustomEase.create("ease-out-cubic", "0.215,0.61,0.355,1");
 
@@ -14,3 +7,11 @@ function initializeAnimation() {
 }
 
 window.addEventListener("DOMContentLoaded", initializeAnimation);
+
+
+// Uncomment to be able to toggle the grid visualizer
+document.addEventListener("keydown", (event) => {
+    if (event.shiftKey && event.key.toLowerCase() === "g") {
+      document.querySelector(".overlay").classList.toggle("active");
+    }
+});
